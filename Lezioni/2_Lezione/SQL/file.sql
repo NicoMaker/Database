13fcb4e7ea2e8833ac65edc2ed54f9d139f4e4f2
NicoMaker <nicola.marano02@gmail.com>
2025-10-01 11:30:53 +0200

    DATI

--- Lezioni/2_Lezione/SQL/file.sql
+++ Lezioni/2_Lezione/SQL/file.sql
@@ -2,3 +2,4 @@ SHOW DATABASES; # Elenca tutti i database disponibili
 CREATE DATABASE nome_database; # Crea un nuovo database
 
 USE nome_database; # Seleziona il database da utilizzare
+show TABLES; # Elenca tutte le tabelle nel database selezionato
