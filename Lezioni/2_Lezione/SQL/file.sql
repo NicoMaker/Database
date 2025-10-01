434909fa7ad5dda532e179067db1bf727c4bfb46
NicoMaker <nicola.marano02@gmail.com>
2025-10-01 11:49:12 +0200

    dati

--- Lezioni/2_Lezione/SQL/file.sql
+++ Lezioni/2_Lezione/SQL/file.sql
@@ -3,3 +3,5 @@ CREATE DATABASE nome_database; # Crea un nuovo database
 
 USE nome_database; # Seleziona il database da utilizzare
 show TABLES; # Elenca tutte le tabelle nel database selezionato
+
+mysql -u root -p; # Accedi a MySQL come utente root
