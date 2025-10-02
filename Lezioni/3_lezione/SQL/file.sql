649d4a5151f2558ee3453684b1ea10f6df9fd0c7
NicoMaker <nicola.marano02@gmail.com>
2025-10-02 14:31:13 +0200

    dati

--- Lezioni/3_lezione/SQL/file.sql
+++ Lezioni/3_lezione/SQL/file.sql
@@ -1,3 +1,8 @@
 CREATE USER ADDRESS 'user' IDENTIFIED BY 'password'; # Crea un nuovo utente
+ALTER USER 'utente' IDENTIFIED BY 'nuova_password'; # Cambia la password di un utente esistente4
 
-ALTER USER 'utente' IDENTIFIED BY 'nuova_password'; # Cambia la password di un utente esistente
+GRANT ALL ON *.* TO 'user' WITH GRANT OPTION; # Concede tutti i privilegi su tutti i database e tabelle all'utente
+
+
+
+SHOW DATABASE; #mostra i database esistenti
