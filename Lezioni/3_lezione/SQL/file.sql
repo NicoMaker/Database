e1a9791fd8b3218dd47744fb39f402b651bfe0d1
NicoMaker <nicola.marano02@gmail.com>
2025-10-02 17:47:45 +0200

    query

--- Lezioni/3_lezione/SQL/file.sql
+++ Lezioni/3_lezione/SQL/file.sql
@@ -459,3 +459,6 @@ WHERE
     age BETWEEN 6
     AND 10;
 
+SELECT CONCAT(lastname," ",firstname, "-",age)AS nome_completo FROM students;
+
+SELECT CONCAT(lastname," ",firstname, "-",age)AS nome_completo FROM students;
