f1fb5ec696c9650d48999a2e510ae4ff54d9482c
NicoMaker <nicola.marano02@gmail.com>
2025-10-13 14:55:22 +0200

    dati

---
+++ Lezioni/5_lezione/SQL/file.sql
@@ -0,0 +1,52 @@
+# Studenti ordinati per nome
+SELECT * FROM students ORDER BY firstname ASC;
+
+# corsi con nomi unici
+SELECT DISTINCT coursename FROM courses;
+
+# contare il numero di iscritti per ogni corso 
+SELECT 
+    (SELECT CourseName FROM Courses c WHERE c.CourseID = e.CourseID) AS CourseName,
+    COUNT(e.StudentID) AS NumeroIscritti
+FROM Enrollments e
+GROUP BY e.CourseID
+ORDER BY CourseName;
+
+
+# selezionare gli studenti con voto superiore a 80 ordinato in modo decrescente 
+SELECT 
+    (SELECT s.FirstName FROM Students s WHERE s.StudentID = e.StudentID) AS FirstName,
+    (SELECT s.LastName FROM Students s WHERE s.StudentID = e.StudentID) AS LastName,
+    e.Grade
+FROM Enrollments e
+WHERE e.Grade > 80
+ORDER BY e.Grade DESC;
+
+
+# selezionare i corsi con più di 2 iscrizioni e ordinati per nome
+SELECT *
+FROM Courses
+WHERE CourseID IN (
+    SELECT CourseID
+    FROM Enrollments
+    GROUP BY CourseID
+    HAVING COUNT(StudentID) > 2
+)
+ORDER BY CourseName;
+
+
+# selezionare gli studenti che non hanno punteggi (null) e ordinati per cognome
+SELECT *
+FROM Students
+WHERE StudentID IN (
+    SELECT StudentID
+    FROM Enrollments
+    WHERE Grade IS NULL
+)
+ORDER BY LastName;
+
+# selezionare i corsi che iniziano per C ordinati per nome
+SELECT *
+FROM Courses
+WHERE CourseName LIKE 'C%'
+ORDER BY CourseName;
