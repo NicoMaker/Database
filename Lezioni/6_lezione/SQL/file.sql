93bd1177fd5afe5bb1b0023f3871d1c7f83a76a2
NicoMaker <nicola.marano02@gmail.com>
2025-10-16 10:42:08 +0200

    dati

--- Lezioni/6_lezione/SQL/file.sql
+++ Lezioni/6_lezione/SQL/file.sql
@@ -1,6 +1,4 @@
-source c:\mysqlsampledatabase.sql # esegue tutti i comandi presenti nel file
-
-
+source c: \ mysqlsampledatabase.sql # esegue tutti i comandi presenti nel file
 SELECT
     customername,
     city
@@ -9,27 +7,22 @@ FROM
 ORDER By
     customername;
 
+DELIMITER $ $ CREATE PROCEDURE GetCustomers() BEGIN
+SELECT
+    customername,
+    city,
+    state,
+    postalcode,
+    country
+FROM
+    customers
+ORDER By
+    customername;
 
-DELIMITER $$
-CREATE PROCEDURE GetCustomers()
-BEGIN
-    SELECT
-        customername,
-        city,
-        state,
-        postalcode,
-        country
-    FROM
-        customers
-    ORDER By
-        customername;
-END $$
-DELIMITER ;
+END $ $ DELIMITER;
 
 # eliminazione procedura
-DROP PROCEDURE GetCustomers
-
-# chiamata procedura
+DROP PROCEDURE GetCustomers # chiamata procedura
 CALL GetCustomers();
 
 # eliminazione con if exists
@@ -37,19 +30,49 @@ DROP PROCEDURE IF EXISTS PIPPO;
 
 SHOW warnings;
 
-DELIMITER $$
-CREATE PROCEDURE GetEmployess()
-BEGIN
-    SELECT
-        firstname,
-        lastname,
-        city,
-        state,
-        country
-    FROM
-        employees
+DELIMITER $ $ CREATE PROCEDURE GetEmployess() BEGIN
+SELECT
+    firstname,
+    lastname,
+    city,
+    state,
+    country
+FROM
+    employees
     INNER JOIN offices USING (officeCode);
-END $$
-DELIMITER ;
+
+END $ $ DELIMITER;
 
 CALL GetEmployess();
+
+DELIMITER / / CREATE PROCEDURE GetOfficeByCountry(in countryName varchar(255)) BEGIN
+SELECT
+    *
+FROM
+    offices
+WHERE
+    country = countryName;
+
+END / / DELIMITER;
+
+CALL GetOfficeByCountry('USA');
+
+DELIMITER / / CREATE PROCEDURE GetOfficeByCountries(IN countryList VARCHAR(255)) BEGIN
+SET
+    @query = CONCAT(
+        'SELECT * FROM offices WHERE country IN (''',
+        REPLACE(countryList, ',', ''','''),
+        ''')'
+    );
+
+PREPARE stmt
+FROM
+    @query;
+
+EXECUTE stmt;
+
+DEALLOCATE PREPARE stmt;
+
+END / / DELIMITER;
+
+CALL GetOfficeByCountries('USA,France,Japan');
