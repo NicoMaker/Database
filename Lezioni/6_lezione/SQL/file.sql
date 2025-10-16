c16681c4ac97f50ef5c32438b976a0cae25b0781
NicoMaker <nicola.marano02@gmail.com>
2025-10-16 09:34:43 +0200

    migloria procedura

---
+++ Lezioni/6_lezione/SQL/file.sql
@@ -0,0 +1,26 @@
+source c:\mysqlsampledatabase.sql # esegue tutti i comandi presenti nel file
+
+
+SELECT
+    customername,
+    city
+FROM
+    customers
+ORDER By
+    customername;
+
+
+DELIMITER $$
+CREATE PROCEDURE GetCustomers()
+BEGIN
+    SELECT
+        customername,
+        city,
+        state,
+        postalcode,
+        country
+    FROM
+        customers
+    ORDER By
+        customername;
+END $$
