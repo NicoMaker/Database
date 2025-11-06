434cf4359c61723b08d3c0262ae5e5787a43f551
NicoMaker <nicola.marano02@gmail.com>
2025-11-06 15:44:46 +0100

    dd

--- Lezioni/8_lezione/SQL/file.sql
+++ Lezioni/8_lezione/SQL/file.sql
@@ -86,3 +86,12 @@ DELIMITER ;
 
 
 CALL GetAllCustomerLevels();
+
+
+CREATE TABLE employees_audit (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    employeeNumber INT NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    changedat DATETIME DEFAULT NULL,
+    action VARCHAR(50) DEFAULT NULL
+);
