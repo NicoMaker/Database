ec626fc5f6eb63c4387e8564cb4df50468792289
NicoMaker <nicola.marano02@gmail.com>
2025-11-12 10:26:16 +0100

    ff

--- Lezioni/9_Lezione/SQL/file.sql
+++ Lezioni/9_Lezione/SQL/file.sql
@@ -1,21 +1,20 @@
-CREATE TABLE
-    students3 (
-        studentid SMALLINT (5),
-        firstname VARCHAR(20) 
-    );
+CREATE TABLE students3 (
+    studentid SMALLINT(5),
+    firstname VARCHAR(20)
+);
 
 alter table students3 rename students4;
 
+alter table students4 ADD lastname NVARCHAR (30);
 
-alter table students4 ADD
-    lastname  NVARCHAR(30);
+ALTER TABLE students4
+RENAME COLUMN firstname TO nome,
+RENAME COLUMN lastname TO cognome;
 
+alter table students4 modify COLUMN nome nvarchar (40) NOT NULL;
 
-ALTER TABLE
-    students4 RENAME COLUMN firstname TO nome,
-    RENAME COLUMN lastname TO cognome;
+insert into subjects (title) values ('English');
 
-alter table
-    students4
-modify
-    COLUMN nome nvarchar(40) NOT NULL;
+DELETE FROM subjects WHERE subjectid = 7;
+
+ALTER TABLE subjects MODIFY COLUMN title VARCHAR(50) UNIQUE;
