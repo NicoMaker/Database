fe72f6e1097de04c4424e5c7249e8f411ed0f138
NicoMaker <nicola.marano02@gmail.com>
2025-11-12 11:43:40 +0100

    rr

--- Lezioni/9_Lezione/SQL/file.sql
+++ Lezioni/9_Lezione/SQL/file.sql
@@ -1,7 +1,5 @@
-CREATE TABLE students3 (
-    studentid SMALLINT(5),
-    firstname VARCHAR(20)
-);
+CREATE TABLE
+    students3 (studentid SMALLINT (5), firstname VARCHAR(20));
 
 alter table students3 rename students4;
 
@@ -13,14 +11,32 @@ RENAME COLUMN lastname TO cognome;
 
 alter table students4 modify COLUMN nome nvarchar (40) NOT NULL;
 
-insert into subjects (title) values ('English');
+insert into
+    subjects (title)
+values
+    ('English');
 
-DELETE FROM subjects WHERE subjectid = 7;
+DELETE FROM subjects
+WHERE
+    subjectid = 7;
 
 ALTER TABLE subjects MODIFY COLUMN title VARCHAR(50) UNIQUE;
 
 alter table teachers
-add column subjectid INT(11) NOT NULL DEFAULT(1);
+add column subjectid INT (11) NOT NULL DEFAULT (1);
 
-ALTER TABLE teachers
-ADD CONSTRAINT fk_subjectid FOREIGN KEY (subjectid) REFERENCES subjects (subjectid);
+ALTER TABLE teachers ADD CONSTRAINT fk_subjectid FOREIGN KEY (subjectid) REFERENCES subjects (subjectid);
+
+CREATE USER maker IDENTIFIED BY 'maker1234'
+    -> ;
+CREATE USER authorized IDENTIFIED BY 'auth1234';
+
+GRANT
+SELECT
+,
+    CREATE ON school.* TO maker;
+
+GRANT
+SELECT
+,
+    CREATE ON school.* TO authorized;
